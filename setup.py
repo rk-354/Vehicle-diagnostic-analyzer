24f4c9f4772e36469ad8ec530ba529df4af038f1
RITTIJA KASHYAP <94633797+rk-354@users.noreply.github.com>
2025-04-16 13:44:47 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Vehicle_Diagnostic_Analyzer',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Rittija Kashyap',
+    author_email='rtj.kashyap1@gmail.com',
+    description='Simple script to create aggregated plots from Vehicle Diagnostic OBD-II log data.',
+    install_requires=["pandas", "matplotlib"]
+)
